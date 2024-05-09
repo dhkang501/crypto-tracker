292ef19383231080c013aa15e0de743f0433aa44
dhkang501 <kangg5@naver.com>
2024-05-09 15:06:17 +0900

    price에 candlestick 차트 추가

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,5 +1,71 @@
-function Price() {
-    return <h1>Price</h1>;
+import ApexChart from "react-apexcharts";
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import { log } from "console";
+import { logDOM } from "@testing-library/react";
+
+interface ChartProps{
+  coinId: string;
+}
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+function Price({coinId}:ChartProps) {
+  const {isLoading, data} = useQuery<IHistorical[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    {
+      refetchInterval: 10000,//10초
+    }
+  );
+  const coinData = data?.map((i => {
+    return {
+      x: i.time_close,
+      y: [i.open, i.high, i.low, i.close]
+    };
+  })) ?? [];
+
+
+    return (
+      <div>
+        <ApexChart 
+          type="candlestick"
+          series={[
+            {          
+              data: coinData
+            },
+          ]}
+          options={{
+            chart: {
+              type: 'candlestick',
+              height: 350
+            },
+            title: {
+              text: 'CandleStick Chart',
+              align: 'left'
+            },
+            xaxis: {
+              type: 'datetime'
+            },
+            yaxis: {
+              tooltip: {
+                enabled: true
+              }
+            }
+          }}
+          height={350}
+        />
+      </div>
+    );
   }
   
   export default Price;
--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,5 @@
 import { DefaultTheme } from "styled-components"
 
-// export const theme: DefaultTheme ={
-//     bgColor: "#2f3640",
-//     textColor: "#f5f6fa",
-//     accentColor: "#44bd32",
-// }
-
 export const lightTheme: DefaultTheme ={
     bgColor: "whitesmoke",
     textColor: "black",
